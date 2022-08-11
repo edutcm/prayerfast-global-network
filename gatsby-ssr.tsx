6f0cf0f00211675b07dbfffb1dc82b281a8adf87
Kaleb Heitzman <kaleb@jamfire.io>
2022-08-11 17:47:40 -0400

    fixed firebase provider

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -3,16 +3,8 @@ import loadable from "@loadable/component";
 
 import "./src/styles/global.css";
 
-const FirebaseProvider = loadable(
-  () => import("./src/services/firebase/firebase-provider")
-);
-
 const AppProvider = loadable(() => import("./src/services/app/app-provider"));
 
 export const wrapRootElement = ({ element }) => {
-  return (
-    <FirebaseProvider>
-      <AppProvider>{element}</AppProvider>
-    </FirebaseProvider>
-  );
+  return <AppProvider>{element}</AppProvider>;
 };
