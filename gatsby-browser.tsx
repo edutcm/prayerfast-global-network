61d34ba125817027929b229af4df277c0e477c75
Kaleb Heitzman <kaleb@jamfire.io>
2022-08-11 17:58:02 -0400

    Refactored for Gatsby Cloud (#1)
    * refactored for gatsby cloud
    
    * removed netlify references
    
    * fixed firebase provider

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -3,16 +3,8 @@ import loadable from "@loadable/component";
 
 import "./src/styles/global.css";
 
-const FirebaseProvier = loadable(
-  () => import("./src/services/firebase/firebase-provider")
-);
-
 const AppProvider = loadable(() => import("./src/services/app/app-provider"));
 
 export const wrapRootElement = ({ element }) => {
-  return (
-    <FirebaseProvier>
-      <AppProvider>{element}</AppProvider>
-    </FirebaseProvier>
-  );
+  return <AppProvider>{element}</AppProvider>;
 };
