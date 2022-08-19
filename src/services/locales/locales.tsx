c8c007ed2baa1970cdcaf60d98df2a15f3bce21d
Kaleb Heitzman <kaleb@jamfire.io>
2022-08-19 10:43:42 -0400

    fixed locales

--- src/services/locales/locales.tsx
+++ src/services/locales/locales.tsx
@@ -5,58 +5,58 @@ export const locales = [
     key: "en",
     label: "English",
   },
-  {
-    key: "ar",
-    label: "عربي",
-  },
+  // {
+  //   key: "ar",
+  //   label: "عربي",
+  // },
   {
     key: "bg",
     label: "български",
   },
-  {
-    key: "bn",
-    label: "বাংলা",
-  },
+  // {
+  //   key: "bn",
+  //   label: "বাংলা",
+  // },
   {
     key: "cs",
     label: "Česky",
   },
-  {
-    key: "de",
-    label: "Deutsch",
-  },
-  {
-    key: "es",
-    label: "Español",
-  },
-  {
-    key: "et",
-    label: "Eesti",
-  },
-  {
-    key: "fr",
-    label: "Français",
-  },
-  {
-    key: "hi",
-    label: "हिन्दी",
-  },
-  {
-    key: "hu",
-    label: "Magyar",
-  },
-  {
-    key: "in",
-    label: "Indonesia",
-  },
+  // {
+  //   key: "de",
+  //   label: "Deutsch",
+  // },
+  // {
+  //   key: "es",
+  //   label: "Español",
+  // },
+  // {
+  //   key: "et",
+  //   label: "Eesti",
+  // },
+  // {
+  //   key: "fr",
+  //   label: "Français",
+  // },
+  // {
+  //   key: "hi",
+  //   label: "हिन्दी",
+  // },
+  // {
+  //   key: "hu",
+  //   label: "Magyar",
+  // },
+  // {
+  //   key: "in",
+  //   label: "Indonesia",
+  // },
   {
     key: "pl",
     label: "Polski",
   },
-  {
-    key: "pt",
-    label: "Português",
-  },
+  // {
+  //   key: "pt",
+  //   label: "Português",
+  // },
   {
     key: "ro",
     label: "Românesc",
@@ -69,12 +69,12 @@ export const locales = [
     key: "tr",
     label: "Türk",
   },
-  {
-    key: "uk",
-    label: "українська",
-  },
-  {
-    key: "zh",
-    label: "普通话",
-  },
+  // {
+  //   key: "uk",
+  //   label: "українська",
+  // },
+  // {
+  //   key: "zh",
+  //   label: "普通话",
+  // },
 ];
