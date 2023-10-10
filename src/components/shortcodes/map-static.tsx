fbf873f388c18109be9ca765f800a04ec984e056
Kaleb Heitzman <kaleb@jamfire.io>
2023-10-10 13:23:17 -0400

    updated map

--- src/components/shortcodes/map-static.tsx
+++ src/components/shortcodes/map-static.tsx
@@ -128,13 +128,13 @@ export const StaticMap = ({ children }: MapProps) => {
 
       // map layer
       const Stamen_Watercolor = L.tileLayer(
-        "https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png",
+        "https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}",
         {
-          attribution:
-            'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-          subdomains: "abcd",
           minZoom: 1,
           maxZoom: 16,
+          attribution:
+            '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+          ext: "jpg",
         }
       );
       Stamen_Watercolor.addTo(map);
